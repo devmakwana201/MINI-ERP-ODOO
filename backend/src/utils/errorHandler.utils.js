3c234fa54760b05c7b8fdce1711820e041b941ee
omprajapati1011 <om313989@gmail.com>
2026-06-13 19:03:27 +0530

    Remove Backend folder

--- backend/src/utils/errorHandler.utils.js
+++
@@ -1,8 +0,0 @@
-const createError = (message, statusCode) => {
-  let error = {};
-  error.message = message;
-  error.statusCode = statusCode;
-  return error;
-};
-
-module.exports = { createError };
